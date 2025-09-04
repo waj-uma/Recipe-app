57cb154997b4cf9df64739a4c7ce859d6afe2940
waj-uma <wajiniesshva@gmail.com>
2025-09-04 10:17:33 +0530

    backend implementation and corn added

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-import { ENV } from "./src/config/env.js";
-
-export default{
-    schema:"./src/db/schema.js",
-    out:"./src/migrations",
-    dialect:"postgresql",
-    dbCredentials:{url:ENV.DATABASE_URL},
-};
